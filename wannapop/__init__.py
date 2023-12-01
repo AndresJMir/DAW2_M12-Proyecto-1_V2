09c2b856ee0dd71431b35e8e648c313ec1d89a3a
AndresJMir <anunez1@insjoaquimmir.cat>
2023-12-01 15:59:14 +0100

    solucio profe

--- wannapop/__init__.py
+++ wannapop/__init__.py
@@ -1,44 +1,41 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_principal import Principal
+from .helper_mail import MailManager
 
 db_manager = SQLAlchemy()
 login_manager = LoginManager()
+principal_manager = Principal()
+mail_manager = MailManager()
 
 def create_app():
     # Construct the core app object
     app = Flask(__name__)
-    app.config.from_object('wannapop.config.Config')
-    # Using a production configuration
-    # app.config.from_object('config.ProdConfig')
 
-    # Using a development configuration
-    #app.config.from_object('config.DevConfig')
-
-    # Secret key
-    ##app.config["SECRET_KEY"] = "Valor aleatori molt llarg i super secret"
-
-    # ruta absoluta d'aquesta carpeta
-    #basedir = os.path.abspath(os.path.dirname(__file__)) 
-
-    # paràmetre que farà servir SQLAlchemy per a connectar-se
-    ##app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + basedir + "/../database.db"
-    # mostre als logs les ordres SQL que s'executen
-    #app.config["SQLALCHEMY_ECHO"] = True
+    # Llegeixo la configuració del config.py de l'arrel
+    app.config.from_object('config.Config')
 
     # Inicialitza els plugins
     login_manager.init_app(app)
     db_manager.init_app(app)
-
+    principal_manager.init_app(app)
+    mail_manager.init_app(app)
+    
     with app.app_context():
-        from . import routes_main, routes_auth
+        from . import commands, routes_main, routes_auth, routes_admin, routes_products, routes_category, routes_status
 
         # Registra els blueprints
         app.register_blueprint(routes_main.main_bp)
         app.register_blueprint(routes_auth.auth_bp)
+        app.register_blueprint(routes_admin.admin_bp)
+        app.register_blueprint(routes_products.products_bp)
+        app.register_blueprint(routes_category.category_bp)
+        app.register_blueprint(routes_status.status_bp)
+        
+        # Registra comandes
+        app.cli.add_command(commands.db_cli)
 
     app.logger.info("Aplicació iniciada")
-    #app.logger.debug(app.config)
 
     return app
