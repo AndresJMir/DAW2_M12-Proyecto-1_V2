fdce0094b44048eb2e88e33a8698154ace6d5fe0
AndresJMir <anunez1@insjoaquimmir.cat>
2023-12-15 21:12:22 +0100

    en proceso

--- wannapop/routes_admin.py
+++ wannapop/routes_admin.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, flash
-from .models import User
+from flask import Blueprint, render_template, flash, abort, redirect, url_for
+from .models import User, BlockedUser
 from .helper_role import Role, role_required
+from .forms import DeleteForm
 from . import db_manager as db
 
 # Blueprint
@@ -15,14 +16,34 @@ def admin_index():
 @role_required(Role.admin)
 def admin_users():
     users = db.session.query(User).all()
-    return render_template('admin/users_list.html', users=users)
+    block = db.session.query(BlockedUser).join(User)
+    return render_template('admin/users_list.html', users=users, block=block)
 
 #Rutas para la Moderacion
+
+@admin_bp.route('/admin/bloc')
+@role_required(Role.admin)
+def bad_users():
+    users = db.session.query(User).all()
+    block = db.session.query(BlockedUser).all()
+    return render_template('admin/blocked_users_list.html', users=users, block=block)
 
 @admin_bp.route('/users/<user_id>/block', methods=['POST'])
 @role_required(Role.admin)
 def block_user(user_id):
     # Lógica para bloquear al usuario
+    user = db.session.query(User).filter(User.id == user_id).one_or_none()
+    if not user:
+        abort(404)
+    form = BlockedUser()
+    if form.validate_on_submit(): # si s'ha fet submit al formulari
+        # insert!
+        db.session.add()
+        db.session.commit()
+        flash("Usuario bloqueado", "success")
+        return redirect(url_for('/admin/users'))
+    else: # GET
+        return render_template('products/delete.html', form = form, product = product)
     flash("Usuari bloqueao")
     return "Usuario bloqueado"  # Puedes retornar un mensaje o código de estado apropiado
 
